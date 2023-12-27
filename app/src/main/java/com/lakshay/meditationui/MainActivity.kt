8d8e6ccfcaae719902403ec1738ebfd52f9fe07b
lakshay-deep <dlakshay20@gmail.com>
2023-12-27 12:55:39 +0530

    HomePageUI Completed

--- app/src/main/java/com/lakshay/meditationui/MainActivity.kt
+++ app/src/main/java/com/lakshay/meditationui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lakshay.meditationui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,20 +11,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.lakshay.meditationui.ui.HomeScreen
 import com.lakshay.meditationui.ui.theme.MeditationUITheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MeditationUITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
-                }
+                HomeScreen()
             }
         }
     }
